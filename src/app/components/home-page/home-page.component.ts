75d9240c65887e208bba62255da0a8c47745a5ba
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2018-09-21 12:12:26 +0200

    feat: content and styles for home page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class HomePageComponent {}
